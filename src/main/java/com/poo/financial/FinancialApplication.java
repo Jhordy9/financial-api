2222c39e8c800f63c2a23b7e010bbda8df55e39e
jhordy <jhooordy@gmail.com>
2023-11-21 12:53:13 -0300

    chore: disable cors

--- src/main/java/com/poo/financial/FinancialApplication.java
+++ src/main/java/com/poo/financial/FinancialApplication.java
@@ -2,6 +2,9 @@ package com.poo.financial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinancialApplication {
@@ -10,4 +13,16 @@ public class FinancialApplication {
 		SpringApplication.run(FinancialApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
